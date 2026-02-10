2dd9f234bfabbfbdf6d5c849550c74011cbbc5fe
Jagriti Priya <jagritidevelop21@gmail.com>
2026-02-10 03:13:05 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);
+// and such comparison >= < == !=
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// comparison converts null then compare i.e, 3 is true
+// equality is direct thats why false
+
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+
+// ===
+//strictly check i.e datatypes
+
+console.log("2" === 2);
