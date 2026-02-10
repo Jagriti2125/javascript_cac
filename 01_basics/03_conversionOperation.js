8e33b2c1d691baeb21e5793a14e29708b3258cdf
Jagriti Priya <jagritidevelop21@gmail.com>
2026-02-10 03:03:06 +0000

    conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,28 +1,75 @@
-let score = 33
+// let score = 33
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
-// "33" - 33
-// "33abc" - NaN
-// true - 1
-// null - 0
+// // "33" - 33
+// // "33abc" - NaN
+// // true - 1
+// // null - 0
 
-let isLoggedIn = 1
+// let isLoggedIn = 1
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
 
-// 1 - true 0 - false
-// "" - false
-// "jagriti" - true
+// // 1 - true 0 - false
+// // "" - false
+// // "jagriti" - true
 
-let someNumber = 33
+// let someNumber = 33
 
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log( typeof stringNumber);
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log( typeof stringNumber);
+
+//********** Operations  **********//
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " Jagriti"
+
+let str3 =  str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+// console.log(+"");
+// console.log(true);
+
+let num1 , num2 , num3
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+// read 
+// https://tc39.es/ecma262/2023/multipage/abstract-operations.html
+
+
+
+
+
+
+
+
