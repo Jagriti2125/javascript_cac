139df9c77a742fc47371d984b9ce16919b48651c
Jagriti Priya <jagritidevelop21@gmail.com>
2026-02-10 08:28:20 +0000

    stack heap

--- 01_basics/05_datatypes-summary.js
+++ 01_basics/05_datatypes-summary.js
@@ -2,34 +2,63 @@
 
 // 7 types : String , Number , Boolean , null , undefined , Symbol , BigInt
 
-const score = 100
-const scoreValue = 100.3
+// const score = 100
+// const scoreValue = 100.3
 
-const isLoggedIn = false
-const outsideTemp = null
-let userEmail;
+// const isLoggedIn = false
+// const outsideTemp = null
+// let userEmail;
 
-const id = Symbol('123')
-const anotherId = Symbol('123')
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
 
-//console.log(id == anotherId);
+// //console.log(id == anotherId);
 
-const bigNumber = 98724284521478129n
+// const bigNumber = 98724284521478129n
 
 
 
-// Reference (Non Primitive)
+// // Reference (Non Primitive)
 
-// Array , Objects , Functions
+// // Array , Objects , Functions
 
-const heros = ["shaktiman" , "naagraj" , "doga"];
-let myObj = {
-    name: "jagriti",
-    age: 23,
+// const heros = ["shaktiman" , "naagraj" , "doga"];
+// let myObj = {
+//     name: "jagriti",
+//     age: 23,
+// }
+
+// const myFunction = function(){
+//   console.log("hello hello");   
+// }
+
+// console.log( typeof bigNumber);
+
+
+// ***********************
+
+
+// Stack(primitive) , Heap (non - primitive)
+
+let myYoutubename = "jago jots hehe"
+
+let anothername = myYoutubename
+anothername = "jagriitii"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let user1={
+  email: "user@com",
+  upi: "user@ybl",
 }
 
-const myFunction = function(){
-  console.log("hello hello");   
-}
+let user2 = user1
 
-console.log( typeof bigNumber);
+user2.email = "jago@com"
+
+console.log(user1.email);
+console.log(user2.email);
+
+// stack - copy 
+// heap - reference (original)
