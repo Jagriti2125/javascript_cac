58aaaa8a29a50e775c794603f9db4a0d3b536b93
Jagriti Priya <jagritidevelop21@gmail.com>
2026-04-28 17:18:42 +0000

    control flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,26 @@
+// if
+// const isUserloggedIn = true
+// const temperature = 41
+// if (temperature < 50) {
+//     console.log("less than 50");
+// }
+// console.log("temp");
+
+
+// block scope
+
+// const score =200
+
+// if(score>100){
+//     var power ="fly"
+//     console.log(`user power:${power}`);
+// }
+// console.log(`user power:${power}`);
+
+// shorthand notation
+
+// const bal=1000
+//if (bal>500) console.log("test"),console.log("ye");
+// not right way to write 
+
+
---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,40 @@
+// const userEmail = "j@jago.ai"
+
+// if (userEmail) {
+//     console.log("got user email");
+// }
+// else {
+//      console.log("dont have");
+     
+// }
+
+// falsy values 
+
+// false ,0 ,-0, 
+// BigInt 0n, "", null, undefined,NaN
+
+//TRUTHY
+//"0", 'false'," ",[],{},function(){}
+
+//useremail=[]
+//if(useremail.length === 0){
+// console.log("empty array");
+//}
+
+//const emptyobj ={}
+// if(Object.keys(emptyObj).length === 0){
+// console.log("empty obj");
+// }
+
+//NULLISH COALESCING OPERATOR (??): null undefined
+
+// let val1;
+// //val1= 5 ?? 10
+// //val1 = null ?? 10
+// val1 = undefined ?? 15
+
+// console.log(val1);
+
+// TERNIARY OPERATOR
+
+//condition ? true : false
