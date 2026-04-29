2e7601cf353b20ab45862e27b58618ff375080b9
Jagriti Priya <jagritidevelop21@gmail.com>
2026-04-29 11:25:45 +0000

    for in of each

---
+++ 05_iterations/five.js
@@ -0,0 +1,41 @@
+const coding = ["js","ruby","java","python","cpp"]
+
+// coding.forEach( function (item) {
+//      console.log(item);
+     
+// }) 
+
+// coding.forEach((item)=> {
+//     console.log(item);
+    
+// })
+
+// function printMe(item){
+//     console.log(item);
+    
+// }
+// coding.forEach(printMe)
+
+// coding.forEach((item, index ,arr)=>{
+//     console.log(item,index,arr);
+    
+// })
+
+const mycoding = [
+    {
+        languagename: "javascript",
+        languagefile:"js"
+    },
+    {
+        languagename: "java",
+        languagefile:"java"
+    },
+    {
+        languagename: "python",
+        languagefile:"py"
+    }
+]
+mycoding.forEach((item)=>{
+    console.log(item.languagename);
+    
+})
